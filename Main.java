3174b1e8ab39e1618c1fcac4f7168153d6590d6a
harshada-patill <harshadapatil8725@gmail.com>
2025-12-09 19:26:24 +0530

    Object Initialization through Reference Variable

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+class Student {
+    int age;
+    String name;
+}
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("shiv");
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.age = 20;
+        s.name = "shiv";
+        System.out.println("my name is " + s.name);
     }
 }
